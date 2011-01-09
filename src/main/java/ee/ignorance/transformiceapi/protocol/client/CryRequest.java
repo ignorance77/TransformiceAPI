b79999204405cda32ef4b0915c2a2951ff188b55
unknown <user@.(none)>
2011-01-09 15:31:41 +0100

    Added cry request

---
+++ src/main/java/ee/ignorance/transformiceapi/protocol/client/CryRequest.java
@@ -0,0 +1,29 @@
+package ee.ignorance.transformiceapi.protocol.client;
+
+import ee.ignorance.transformiceapi.protocol.ByteBuffer;
+
+public class CryRequest extends AbstractClientRequest {
+
+        private String gameCode;
+
+        public CryRequest(String gameCode) {
+                this.gameCode = gameCode;
+        }
+
+        @Override
+        public char[] getBytes() {
+                ByteBuffer bf = new ByteBuffer();
+                bf.write(0x06);
+                bf.write(0x1a);
+                bf.write(0x01);
+                bf.write(0x70);
+                bf.write(0x6c);
+                bf.write(0x65);
+                bf.write(0x75);
+                bf.write(0x72);
+                bf.write(0x65);
+                bf.write(0x72);
+                bf.write(0x00);
+                return bf.getBytesChar();
+        }
+}
