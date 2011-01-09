bcbd1937ae3fd736b581a803b1bc2ecf68acd809
unknown <user@.(none)>
2011-01-09 15:37:53 +0100

    Added death request

---
+++ src/main/java/ee/ignorance/transformiceapi/protocol/client/DeathRequest.java
@@ -0,0 +1,21 @@
+package ee.ignorance.transformiceapi.protocol.client;
+
+import ee.ignorance.transformiceapi.protocol.ByteBuffer;
+
+public class DeathRequest extends AbstractClientRequest {
+
+        private String gameCode;
+
+        public DeathRequest(String gameCode) {
+                this.gameCode = gameCode;
+        }
+
+        @Override
+        public char[] getBytes() {
+                ByteBuffer bf = new ByteBuffer();
+                bf.write(0x04);
+                bf.write(0x05);
+                bf.write(0x00);
+                return bf.getBytesChar();
+        }
+}
