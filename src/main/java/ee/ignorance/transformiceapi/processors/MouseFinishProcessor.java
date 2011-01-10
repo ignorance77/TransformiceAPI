5e477540d39b6d8eb98f28a098e92e9216006c50
Aleksandr <ignorance77@gmail.com>
2011-01-10 23:25:10 +0200

    more events

--- src/main/java/ee/ignorance/transformiceapi/processors/MouseFinishProcessor.java
+++ src/main/java/ee/ignorance/transformiceapi/processors/MouseFinishProcessor.java
@@ -1,6 +1,7 @@
 package ee.ignorance.transformiceapi.processors;
 
 import ee.ignorance.transformiceapi.Player;
+import ee.ignorance.transformiceapi.event.MouseFinishEvent;
 import ee.ignorance.transformiceapi.protocol.server.AbstractResponse;
 import ee.ignorance.transformiceapi.protocol.server.MouseFinishResponse;
 
@@ -9,7 +10,8 @@ public class MouseFinishProcessor extends CommandProcessor{
 
     @Override
     public void process(AbstractResponse command, Player player) {
-        /* TODO when the new event system is merged */
+    	MouseFinishResponse response = (MouseFinishResponse) command;
+    	player.notifyListeners(new MouseFinishEvent( response.getMouseID(), response.getStanding(), response.getFinishTime()));
     }
 
 
