d2aa9dfbebf3e6ae4a46f6df56f77a408342e193
unknown <user@.(none)>
2011-01-10 20:46:15 +0100

    mouse finish stuff

---
+++ src/main/java/ee/ignorance/transformiceapi/processors/MouseFinishProcessor.java
@@ -0,0 +1,16 @@
+package ee.ignorance.transformiceapi.processors;
+
+import ee.ignorance.transformiceapi.Player;
+import ee.ignorance.transformiceapi.protocol.server.AbstractResponse;
+import ee.ignorance.transformiceapi.protocol.server.MouseFinishResponse;
+
+
+public class MouseFinishProcessor extends CommandProcessor{
+
+    @Override
+    public void process(AbstractResponse command, Player player) {
+        /* TODO when the new event system is merged */
+    }
+
+
+}
