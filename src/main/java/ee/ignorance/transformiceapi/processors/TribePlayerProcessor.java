cfd3426cd545478b655aafb362e54d94ec13a9dc
Fsefsfsef <transformice@befog.net>
2011-01-15 00:11:59 +0100

    add TribePlayerDisconnect event

--- src/main/java/ee/ignorance/transformiceapi/processors/TribePlayerProcessor.java
+++ src/main/java/ee/ignorance/transformiceapi/processors/TribePlayerProcessor.java
@@ -2,6 +2,7 @@ package ee.ignorance.transformiceapi.processors;
 
 import ee.ignorance.transformiceapi.Player;
 import ee.ignorance.transformiceapi.event.TribePlayerConnectEvent;
+import ee.ignorance.transformiceapi.event.TribePlayerDisconnectEvent;
 import ee.ignorance.transformiceapi.protocol.server.AbstractResponse;
 import ee.ignorance.transformiceapi.protocol.server.TribePlayerResponse;
 
@@ -12,8 +13,10 @@ public class TribePlayerProcessor extends CommandProcessor{
     public void process(AbstractResponse command, Player player) {
         TribePlayerResponse response = (TribePlayerResponse) command;
         if(response.getType() == 1){
-            System.out.println("creating TribePlayerConnect");
             player.notifyListeners(new TribePlayerConnectEvent(response.getPlayerName()));
+        }
+        if(response.getType() == 2){
+            player.notifyListeners(new TribePlayerDisconnectEvent(response.getPlayerName()));
         }
     }
 }
