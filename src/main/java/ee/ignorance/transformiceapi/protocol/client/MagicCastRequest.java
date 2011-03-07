61056d505d41ffeb9300b95df833532e32815209
icewolf <ice.wolf@live.co.uk>
2011-03-07 12:31:34 -0800

    MagicCastRequest Protocol changes

--- src/main/java/ee/ignorance/transformiceapi/protocol/client/MagicCastRequest.java
+++ src/main/java/ee/ignorance/transformiceapi/protocol/client/MagicCastRequest.java
@@ -1,5 +1,7 @@
 package ee.ignorance.transformiceapi.protocol.client;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import ee.ignorance.transformiceapi.protocol.ByteBuffer;
 
 
@@ -18,24 +20,30 @@ public class MagicCastRequest extends AbstractClientRequest {
 	
 	@Override
 	public char[] getBytes() {
-		ByteBuffer bf = new ByteBuffer();
-		bf.write(0x05);
-		bf.write(20);
-		bf.write(0x01);
-		bf.print("" + code);
-		bf.write(0x01);
-		bf.print("" + x);
-		bf.write(0x01);
-		bf.print("" + y);
-		bf.write(0x01);
-		bf.print("0");
-		bf.write(0x01);
-		bf.print("0");
-		bf.write(0x01);
-		bf.print("0");
-		bf.write(0x01);
-		bf.print("1");
-		return bf.getBytesChar();
+		ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(byteOut);
+            try {
+                out.writeByte(5);
+                out.writeByte(20);
+                out.writeShort(code);
+                out.writeShort(-1); //Object Identifier? Using -1 works fine 
+                out.writeShort(x);
+                out.writeShort(y);
+                out.writeShort(0); //Rotation
+                out.writeByte(0); //x Velocity
+                out.writeByte(0); //Y Velocity
+                out.writeByte(1); // ?
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            byte[] bytes = byteOut.toByteArray();
+            char[] chars = new char[bytes.length];
+            for (int i = 0; i < chars.length; i++) {
+                chars[i] = (char) bytes[i];
+            }
+
+            return chars;
 	}
 
 }
