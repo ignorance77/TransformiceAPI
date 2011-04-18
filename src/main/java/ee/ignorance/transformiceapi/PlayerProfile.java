75f7a043b9933564551b02b61e7e14475c364940
nomnombacon <nomnombacon@hotmail.com>
2011-04-18 19:09:37 +0200

    formatting & toString()

--- src/main/java/ee/ignorance/transformiceapi/PlayerProfile.java
+++ src/main/java/ee/ignorance/transformiceapi/PlayerProfile.java
@@ -4,106 +4,117 @@ import ee.ignorance.transformiceapi.titles.Titles;
 
 public class PlayerProfile {
 
-    private String playerName;
-    private String tribe;
-    private int saves;
-    private int hardSaves;
-    private int personalGathered;
-    private int firsts;
-    private int cheese;
+        private String playerName;
+        private String tribe;
+        private int saves;
+        private int hardSaves;
+        private int personalGathered;
+        private int firsts;
+        private int cheese;
 
-    public PlayerProfile(String playerName, String tribe, int saves, int hardSaves, int personalGathered, int firsts, int cheese) {
-        this.playerName = playerName;
-        this.tribe = tribe;
-        this.saves = saves;
-        this.hardSaves = hardSaves;
-        this.personalGathered = personalGathered;
-        this.firsts = firsts;
-        this.cheese = cheese;
-    }
+        public PlayerProfile(String playerName, String tribe, int saves, int hardSaves, int personalGathered, int firsts, int cheese) {
+                this.playerName = playerName;
+                this.tribe = tribe;
+                this.saves = saves;
+                this.hardSaves = hardSaves;
+                this.personalGathered = personalGathered;
+                this.firsts = firsts;
+                this.cheese = cheese;
+        }
 
-    public String getPlayer() {
-        return playerName;
-    }
+        public String getPlayer() {
+                return playerName;
+        }
 
-    public void setPlayer(String playerName) {
-        this.playerName = playerName;
-    }
+        public void setPlayer(String playerName) {
+                this.playerName = playerName;
+        }
 
-    public String getTribe() {
-        return tribe;
-    }
+        public String getTribe() {
+                return tribe;
+        }
 
-    public void setTribe(String tribe) {
-        this.tribe = tribe;
-    }
+        public void setTribe(String tribe) {
+                this.tribe = tribe;
+        }
 
-    public int getSaves() {
-        return saves;
-    }
+        public int getSaves() {
+                return saves;
+        }
 
-    public void setSaves(int saves) {
-        this.saves = saves;
-    }
+        public void setSaves(int saves) {
+                this.saves = saves;
+        }
 
-     public int getHardSaves() {
-        return hardSaves;
-    }
+        public int getHardSaves() {
+                return hardSaves;
+        }
 
-    public void setHardSaves(int hardSaves) {
-        this.hardSaves = hardSaves;
-    }
+        public void setHardSaves(int hardSaves) {
+                this.hardSaves = hardSaves;
+        }
 
-    public int getPersonalGathered() {
-        return personalGathered;
-    }
+        public int getPersonalGathered() {
+                return personalGathered;
+        }
 
-    public void setPersonalGathered(int personalGathered) {
-        this.personalGathered = personalGathered;
-    }
+        public void setPersonalGathered(int personalGathered) {
+                this.personalGathered = personalGathered;
+        }
 
-    public int getFirsts() {
-        return firsts;
-    }
+        public int getFirsts() {
+                return firsts;
+        }
 
-    public void setFirsts(int firsts) {
-        this.firsts = firsts;
-    }
+        public void setFirsts(int firsts) {
+                this.firsts = firsts;
+        }
 
-    public int getCheese() {
-        return cheese;
-    }
+        public int getCheese() {
+                return cheese;
+        }
 
-    public void setCheese(int cheese) {
-        this.cheese = cheese;
-    }
+        public void setCheese(int cheese) {
+                this.cheese = cheese;
+        }
 
-    /* Some interesting information about upcoming titles */
+        /* Some interesting information about upcoming titles */
+        public String nextCheeseTitle() {
+                return Titles.nextCheeseTitle(cheese);
+        }
 
-    public String nextCheeseTitle() {
-        return Titles.nextCheeseTitle(cheese);
-    }
-    public String nextFirstsTitle() {
-        return Titles.nextFirstsTitle(firsts);
-    }
-    public String nextSavesTitle() {
-        return Titles.nextSavesTitle(saves);
-    }
-    public String nextHardSavesTitle() {
-        return Titles.nextHardSavesTitle(hardSaves);
-    }
+        public String nextFirstsTitle() {
+                return Titles.nextFirstsTitle(firsts);
+        }
 
-    public int cheeseForNextCheeseTitle() {
-        return Titles.cheeseForNextCheeseTitle(cheese);
-    }
-    public int firstsForNextFirstsTitle() {
-        return Titles.firstsForNextFirstsTitle(firsts);
-    }
-    public int savesForNextSavesTitle() {
-        return Titles.savesForNextSavesTitle(saves);
-    }
-    public int hardSavesForNextHardSavesTitle() {
-        return Titles.hardSavesForNextHardSavesTitle(hardSaves);
-    }
+        public String nextSavesTitle() {
+                return Titles.nextSavesTitle(saves);
+        }
 
+        public String nextHardSavesTitle() {
+                return Titles.nextHardSavesTitle(hardSaves);
+        }
+
+        public int cheeseForNextCheeseTitle() {
+                return Titles.cheeseForNextCheeseTitle(cheese);
+        }
+
+        public int firstsForNextFirstsTitle() {
+                return Titles.firstsForNextFirstsTitle(firsts);
+        }
+
+        public int savesForNextSavesTitle() {
+                return Titles.savesForNextSavesTitle(saves);
+        }
+
+        public int hardSavesForNextHardSavesTitle() {
+                return Titles.hardSavesForNextHardSavesTitle(hardSaves);
+        }
+
+        @Override
+        public String toString() {
+                return "PlayerProfile [playerName=" + playerName + ", tribe=" + tribe + ", saves=" + saves
+                        + ", hardSaves=" + hardSaves + ", personalGathered=" + personalGathered
+                        + ", first=" + firsts + ", cheese=" + cheese + "]";
+        }
 }
